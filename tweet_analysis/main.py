d39adf030778b7b2faba6b8771817562a2095c3b
Luke Ireland <luke.ireland@arm.com>
2020-02-06 17:34:00 +0000

    Added timing to classification methods

--- tweet_analysis/main.py
+++ tweet_analysis/main.py
@@ -1,3 +1,6 @@
+from timeit import default_timer as timer
+
+import numpy as np
 from sklearn.model_selection import cross_val_score
 
 from algorithms.logistic_regression import LogisticRegression
@@ -6,10 +9,23 @@ from algorithms.random_forest import RandomForest
 from algorithms.xg_boost import XGBoost
 from utils.tweet_getter import TweetGetter
 
-import numpy as np
 
 def avg(scores):
     return np.average(scores)
+
+
+def wrapper(func, *args, **kwargs):
+    def wrapped():
+        return func(*args, **kwargs)
+    return wrapped
+
+
+def time(function):
+    start = timer()
+    score = function()
+    time = timer() - start
+    return score, time
+
 
 tweets = TweetGetter().get_clean_tweets_with_scores()
 X = tweets.tweet
@@ -20,14 +36,28 @@ logistic_regression = LogisticRegression()
 random_forest = RandomForest()
 multilayer_perceptron = MultilayerPerceptron()
 
-xg_boost_score = cross_val_score(
-    xg_boost.pipeline, X, y, cv=5, scoring='accuracy')
-logistic_regression_score = scores = cross_val_score(
-    logistic_regression.pipeline, X, y, cv=5, scoring='accuracy')
-random_forest_score = cross_val_score(
-    random_forest.pipeline, X, y, cv=5, scoring='accuracy')
-multilayer_perceptron_score = cross_val_score(
-    multilayer_perceptron.pipeline, X, y, cv=5, scoring='accuracy')
+xg_boost_func = wrapper(
+    cross_val_score, xg_boost.pipeline, X, y, cv=5, scoring='accuracy')
+logistic_regression_func = wrapper(
+    cross_val_score, logistic_regression.pipeline, X, y, cv=5, scoring='accuracy')
+random_forest_func = wrapper(
+    cross_val_score, random_forest.pipeline, X, y, cv=5, scoring='accuracy')
+multilayer_perceptron_func = wrapper(
+    cross_val_score, multilayer_perceptron.pipeline, X, y, cv=5, scoring='accuracy')
+
+xg_boost_score, xg_boost_time = time(xg_boost_func)
+logistic_regression_score, logistic_regression_time = time(
+    logistic_regression_func)
+random_forest_score, random_forest_time = time(random_forest_func)
+multilayer_perceptron_score, multilayer_perceptron_time = time(
+    multilayer_perceptron_func)
+
+times = dict()
+times["XG Boost"] = xg_boost_time
+times["Logistic Regression"] = logistic_regression_time
+times["Random Forest"] = random_forest_time
+times["Multilayer Perceptron"] = multilayer_perceptron_time
+times = {k: v for k, v in sorted(times.items(), key=lambda item: item[1])}
 
 scores = dict()
 scores["XG Boost"] = avg(xg_boost_score)
@@ -37,4 +67,4 @@ scores["Multilayer Perceptron"] = avg(multilayer_perceptron_score)
 scores = {k: v for k, v in sorted(scores.items(), key=lambda item: item[1])}
 
 for name, score in scores.items():
-    print("Name:\t{}\tScore:\t{}".format(name, avg(score)))
+    print("Name:\t{}\tScore:\t{}\tTime:\t{}".format(name, score, times[name]))
